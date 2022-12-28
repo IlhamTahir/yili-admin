20547519fc944187d6d6f3ee3ef4c1afcadc0300
依力 <yantaq@bilig.biz>
2022-12-28 22:57:01 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import vue from "@vitejs/plugin-vue";
+const BASE = process.env.BASE || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE,
   plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
